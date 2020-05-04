5d3e2a09e8f633f1f4afff529f0fb83880860c79
Victor Guimaraes <victor.gmr@hotmail.com>
2020-05-03 23:44:29 -0300

    updated index message catalogo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div style="position:absolute; top: 50%; left:50%; transform:translate(-50%, -50%);"class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Welcome to the Catalogo Laravel</h1>
+    <h1 class="display-4">Welcome to the Catalogo PurePHP</h1>
     <p class="lead">A simple product management application.</p>
   </div>
 </div>
